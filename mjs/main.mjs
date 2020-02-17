dabd0e2ad078c84d8b278b3af56a730bd0b998b6
Maude Martel <e1678013@cmaisonneuve.qc.ca>
2020-02-17 17:14:08 -0500

    animation livraison

--- mjs/main.mjs
+++ mjs/main.mjs
@@ -1,5 +1,10 @@
 import {anim_entete} from "./anim_entete.mjs";
+import {anim_livraison} from "./anim_livraison.mjs";
+
 let elmEntete = document.querySelector("header .titre");
 const animEntete = new anim_entete(elmEntete);
 animEntete.anim();
 
+let elmLivraison = document.querySelector(".livraison #icon_truck");
+const animCamion = new anim_livraison(elmLivraison);
+animCamion.anim();
